d60a22e77893716c7b6eea703fb37c19afa0be40
pritam dutta <webdevpritam7498@gmail.com>
2024-05-20 23:26:14 +0600

    updated

--- scratch/script.js
+++ scratch/script.js
@@ -1,12 +1,33 @@
 // object
-const product = {
-    name: 'Product 1',
-    price: 10,
+// const product = {
+//     name: 'Product 1',
+//     price: 10,
+//     'product-quantity': '10',
+// }
+
+// product.name = 'socks';
+// product.newProperty = true;
+// delete product.newProperty;
+//dot notation
+// console.log(product.name)
+// bracket notation
+// console.log(product)
+// console.log(product["product-quantity"])
+
+const produtct2 =  {
+    // nested obj
+    bags:{
+        color: 'black',
+        size: 'large',
+
+    },
+    fun: function function1(){
+        console.log('functin inside the object');
+    }
 }
-console.log(product.name)
-product.name = 'socks';
-console.log(product.name)
-product.newProperty = true;
-console.log(product)
-delete product.newProperty;
-console.log(product)
+// console.log(produtct2);
+// console.log(produtct2.bags.size);
+// console.log(produtct2.fun())
+const jsonData = JSON.stringify(produtct2);
+console.log(jsonData)
+console.log(JSON.parse(jsonData));
