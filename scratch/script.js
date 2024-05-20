38966d22633f7e118f1bf45299b80762c833f828
pritam dutta <webdevpritam7498@gmail.com>
2024-05-20 05:56:50 +0200

    updated

--- scratch/script.js
+++ scratch/script.js
@@ -1,2 +1,12 @@
-const variable = "dfdjf";
-console.log(typeof variable)
+// object
+const product = {
+    name: 'Product 1',
+    price: 10,
+}
+console.log(product.name)
+product.name = 'socks';
+console.log(product.name)
+product.newProperty = true;
+console.log(product)
+delete product.newProperty;
+console.log(product)
